8eb3a6e740600cf9886d8f00df6fc46202c684c9
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-03-16 19:39:00 +1100

    Added main to /js folder

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const ratio = width / height;
 const init = () => {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(45, ratio, 1, 1000);
+  camera.position.z = 5;
+  
   renderer = new THREE.WebGLRenderer({ antialias: true });
 
   renderer.setClearColor('#e5e5e5');
@@ -13,14 +15,23 @@ const init = () => {
 
   document.getElementById('webgl').append(renderer.domElement);
 
+  const sphere = getSphere(1, 10, 10, 0xffcc00);
+  scene.add(sphere);
+
+  renderer.render(scene, camera);
+
   window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
   });
+};
 
-  renderer.render(scene, camera);
+const getSphere = (radius, width, height, color) => {
+  let geometry = new THREE.SphereGeometry(radius, width, height);
+  let material = new THREE.MeshLambertMaterial({ color });
+  return new THREE.Mesh(geometry, material);
 };
 
 init();
