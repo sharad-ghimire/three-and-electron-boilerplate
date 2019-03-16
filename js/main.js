a4770286629c2f4b3bb97c67fc8aa9023bba8f06
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-03-16 19:42:56 +1100

    Light added

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const init = () => {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(45, ratio, 1, 1000);
   camera.position.z = 5;
-  
+
   renderer = new THREE.WebGLRenderer({ antialias: true });
 
   renderer.setClearColor('#e5e5e5');
@@ -16,7 +16,10 @@ const init = () => {
   document.getElementById('webgl').append(renderer.domElement);
 
   const sphere = getSphere(1, 10, 10, 0xffcc00);
+  const pointLight = getPointLight(0xffffff, 1, 500);
   scene.add(sphere);
+  scene.add(pointLight);
+  pointLight.position.set(10, 0, 25);
 
   renderer.render(scene, camera);
 
@@ -32,6 +35,11 @@ const getSphere = (radius, width, height, color) => {
   let geometry = new THREE.SphereGeometry(radius, width, height);
   let material = new THREE.MeshLambertMaterial({ color });
   return new THREE.Mesh(geometry, material);
+};
+
+const getPointLight = (color, intensity, distance) => {
+  let light = new THREE.PointLight(color, intensity, distance);
+  return light;
 };
 
 init();
