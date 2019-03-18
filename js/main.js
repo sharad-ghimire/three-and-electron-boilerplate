4913a2306e19e5c11abaecd39c9169958c6569b1
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-03-18 15:45:23 +1100

    OrbitControls and dat.GUI() added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var scene, camera, renderer;
+var scene, camera, renderer, controls, axis, gui;
 const width = window.innerWidth;
 const height = window.innerHeight;
 const ratio = width / height;
@@ -8,6 +8,15 @@ const init = () => {
   camera = new THREE.PerspectiveCamera(45, ratio, 1, 1000);
   camera.position.z = 10;
 
+  // dat.GUI()
+  gui = new dat.GUI();
+  gui.add(camera.position, 'z', 0, 800);
+  // camera Orbit
+  controls = new THREE.OrbitControls(camera);
+  // axis helper
+  axis = new THREE.AxisHelper(300);
+  scene.add(axis);
+
   renderer = new THREE.WebGLRenderer({ antialias: true });
 
   renderer.setClearColor('#e5e5e5');
@@ -15,11 +24,11 @@ const init = () => {
 
   document.getElementById('webgl').append(renderer.domElement);
 
-  const sphere = getSphere(2, 20, 20, 0xffcc00);
-  const pointLight = getPointLight(0xffffff, 1, 500);
+  const sphere = getSphere(50, 32, 16, 0xffcc00);
+  const pointLight = getPointLight(0xffffff, 1, 1000);
   scene.add(sphere);
   scene.add(pointLight);
-  pointLight.position.set(10, 0, 25);
+  pointLight.position.set(200, 0, 25);
 
   window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -28,9 +37,16 @@ const init = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
   });
 
+  const animate = () => {
+    sphere.position.y = 100 * Math.abs(Math.cos(Date.now() * 0.01));
+  };
+
   const render = () => {
     requestAnimationFrame(render);
     renderer.render(scene, camera);
+    controls.update();
+    animate();
+    gui.open();
   };
   render();
 };
